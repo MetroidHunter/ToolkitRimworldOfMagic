ab948086d5cea01c6cd0eed4407d2b19a6371de2
Brys Sepulveda <bksepulv@gmail.com>
2019-12-21 14:54:17 -0800

    Initial commit

---
+++ Source/IncidentHelpers.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using TorannMagic;
+
+namespace ToolkitRimOfMagicPatch
+{
+
+    public class IncidentHelper_ArcaneEnemyRaid : WagerIncidentHelper<TorannMagic.IncidentWorker_ArcaneEnemyRaid>
+    {
+        public IncidentHelper_ArcaneEnemyRaid() : base(IncidentCategoryDefOf.ThreatBig, IncidentDef.Named("ArcaneEnemyRaid")) { }
+    }
+
+    public class IncidentHelper_ArcaneCampQuest : NormalIncidentHelper<TorannMagic.IncidentWorker_QuestArcaneCamp>
+    {
+        public IncidentHelper_ArcaneCampQuest() : base(IncidentCategoryDefOf.WorldQuest, IncidentDef.Named("QuestArcaneCamp")) { }
+    }
+
+    public class IncidentHelper_MagicCaravanRequest : NormalIncidentHelper<TorannMagic.IncidentWorker_ArcaneScriptCaravan>
+    {
+        public IncidentHelper_MagicCaravanRequest() : base(IncidentCategoryDefOf.WorldQuest, IncidentDef.Named("MagicCaravanRequest")) { }
+    }
+
+}
---
+++ Source/ToolkitRimOfMagicPatch.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace ToolkitRimOfMagicPatch
+{
+    public class ToolkitRimOfMagicPatch : Mod
+    {
+        public ToolkitRimOfMagicPatch(ModContentPack content) : base(content)
+        {
+        }
+
+        public override string SettingsCategory() => "Toolkit RimOfMagic Patch";
+    }
+}
